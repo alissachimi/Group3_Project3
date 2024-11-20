76e41b6ac939f364aabee3af373b8bdfdea2eac8
Alissa Chimienti <alissachimienti@gmail.com>
2024-11-19 18:29:37 -0600

    added initial karma stuff

--- calculator/src/app/app.component.spec.ts
+++ calculator/src/app/app.component.spec.ts
@@ -8,22 +8,24 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should clear the display when clear is called', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+    component.appendInput('5');
+    component.appendInput('+');
+    component.appendInput('3');
+    expect(component.display).toBe('5+3');
+    component.clear();
+    expect(component.display).toBe('');
   });
 
-  it(`should have the 'calculator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calculator');
-  });
+//   it('should calculate the result when calculate is called with a valid expression', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const component = fixture.componentInstance;
+//     component.appendInput('5');
+//     component.appendInput('+');
+//     component.appendInput('3');
+//     component.calculate();
+//     expect(component.display).toBe("8");
+//   });
 });
